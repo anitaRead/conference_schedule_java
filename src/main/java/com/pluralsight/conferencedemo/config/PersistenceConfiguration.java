cae64f7be7cf435a0ba5509fb01087cd3863ca30
Anita Readman <anita.readman@sita.aero>
2021-10-18 09:46:08 +0100

    set Maven source and target to Java Version

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,16 +1,20 @@
 //Customise and create changes or configuration in the Persistence tier
 package com.pluralsight.conferencedemo.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
 
-//    @Bean
-//    public DataSource dataSource() {
-//        DataSourceBuilder builder = DataSourceBuilder.create();
-//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-//        System.out.println("My custom datasource bean has been initialised and set");
-//        return builder.build();
-//    }
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        System.out.println("My custom datasource bean has been initialised and set");
+        return builder.build();
+    }
 }
